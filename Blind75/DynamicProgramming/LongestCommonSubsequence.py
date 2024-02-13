33abb2ad3d7dded756ac5cb8ec696a03b2fa56a1
aryocypher <aryocypher@gmail.com>
2024-02-14 01:43:00 +0530

    Longest Common Subsequence Iterative

--- Blind75/DynamicProgramming/LongestCommonSubsequence.py
+++ Blind75/DynamicProgramming/LongestCommonSubsequence.py
@@ -53,3 +53,22 @@ class Solution:
             return cache[i][j]
 
         return lcsrec(0, 0)
+
+    def longestCommonSubsequenceIterative(self, text1: str, text2: str) -> int:
+        l1 = len(text1)
+        l2 = len(text2)
+
+        cache = []
+
+        for i in range(l1+1):
+            curr = [0]*(l2+1)
+            cache.append(curr)
+
+        for i in range(1, l1+1):
+            for j in range(1, l2+1):
+                if text1[i-1] == text2[j-1]:
+                    cache[i][j] = 1+cache[i-1][j-1]
+                else:
+                    cache[i][j] = max(cache[i][j-1], cache[i-1][j])
+
+        return cache[l1][l2]
